681606988f41981f0c4122f901aa4837563553eb
David Inho Lee <davidinholee@gmail.com>
2020-04-20 17:39:01 -0400

    Fixed checkstyle

--- src/main/java/edu/brown/cs/final_project/timagotchi/assignments/Review.java
+++ src/main/java/edu/brown/cs/final_project/timagotchi/assignments/Review.java
@@ -4,7 +4,13 @@ import edu.brown.cs.final_project.timagotchi.Controller;
 import edu.brown.cs.final_project.timagotchi.users.Class;
 import edu.brown.cs.final_project.timagotchi.users.Student;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Review implements Assignment {
   private final int numQuestions = 20;
