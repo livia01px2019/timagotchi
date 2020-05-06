2f697d7d2e35edf8e40e1b747f4065a4e0c6aea1
livia01px2019 <livia.zhu@live.com>
2020-05-06 23:55:50 +0800

    fixing classboard.. again?

--- src/main/java/edu/brown/cs/final_project/timagotchi/Routes.java
+++ src/main/java/edu/brown/cs/final_project/timagotchi/Routes.java
@@ -2,7 +2,6 @@ package edu.brown.cs.final_project.timagotchi;
 
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -283,7 +282,6 @@ public final class Routes {
   public static String generateClassboardHtml(Classboard cb) {
     StringBuilder sb = new StringBuilder();
     List<List<String>> classList = cb.rankClassByTotalXP();
-    Collections.reverse(classList);
     int i = 1;
     if (classList.size() == 0) {
       sb.append("<div class=\"leaderboard-item\"><div class=\"leaderboard-row\">");
